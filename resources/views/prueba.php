866ccd085707fdfe0b3fe71ee33abb22e7ba1292
Jorge Figueroa <U23317023@utp.edu.pe>
2023-07-26 10:17:31 -0500

    update 2

--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -3,3 +3,14 @@ prueba
 jorgefigueroa23
 dfasfas
 dasfafasgfas
+<datagrid>sapi_windows_cp_convas
+
+
+shm_detacha
+<datagrid><datagrid><a href="da
+d
+a
+da
+d
+ad"></a></datagrid></datagrid>
+</datagrid>
